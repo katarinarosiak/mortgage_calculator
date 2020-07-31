9f67a52b4329d974367531fb94ad1468cfd32bad
katarinarosiak <64321232+katarinarosiak@users.noreply.github.com>
2020-07-31 14:11:41 +0200

    Add files via upload

--- mortgage_calculator.js
+++ mortgage_calculator.js
@@ -5,75 +5,88 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+let retriveInput = (promptMessage, invalidMessage, isInvalid) => {
+  prompt(promptMessage);
+  let input = readline.question();
+  while (isInvalid(input)) {
+    prompt(invalidMessage);
+    prompt(promptMessage);
+    input = readline.question();
+  }
+  return input;
+}
+
 function isInvalidNumber(num) {
   return (
     num.toString().trimStart() === "" ||
     Number.isNaN(Number(num)) ||
     Math.sign(num) !== 1
-  );
+  )
+}
+
+function isInvalidAPR(num) {
+  return (
+    num.toString().trimStart() === "" ||
+    Number.isNaN(Number(num)) ||
+    Math.sign(num) === -1
+  )
 }
 
 
 function isInvalidInput(stringYN) {
-  if (stringYN.toLowerCase() === "yes" ||
+  return !(
+    stringYN.toLowerCase() === "yes" ||
     stringYN.toLowerCase() === "no" ||
-    stringYN.trimStart()
-    === "") {
-    return false
-  } else {
-    return true;
-  }
+    stringYN.trimStart() === ""
+  )
 }
+
+let countMonthlyInterestRate = (annRate) => (annRate / 100) / 12;
+
+let countMonthlyPayment = (loanAmount, monthlyInterestRate, loanDurationYears) => {
+  return Number(((loanAmount * (monthlyInterestRate /
+    (1 - Math.pow(1 + monthlyInterestRate, -(loanDurationYears * 12))))).toFixed(2)));
+}
+
+let countTotalPayment = (paymentMonth, loanTerm) => {
+  return (paymentMonth * (loanTerm * 12));
+}
+
+let countTotalInterest = (paymentTotal, loanAmnt) => paymentTotal - loanAmnt;
 
 function printResult(messagee, result) {
   console.log(`=> ${messagee} ${result}`);
 }
 
+let clearScreen = () => {
+  for (let i = 0; i < 22; i = i + 1) {
+    console.log(" ");
+  }
+}
+
+
 prompt(messages.welcome);
 
-
-let countAnother = "yes";
-
-while (countAnother[0] === "y") {
-
-  let retriveInput = (promptMessage, invalidMessage, isInvalid) => {
-    prompt(promptMessage);
-    let input = readline.question();
-    while (isInvalid(input)) {
-      prompt(invalidMessage);
-      prompt(promptMessage);
-      input = readline.question();
-    }
-    return input;
-  }
+while (true) {
 
   let loanAmount = retriveInput(messages.loan, messages.wrongInput, isInvalidNumber);
 
-  let annualPercantageRate = retriveInput(messages.intRate, messages.wrongInput, isInvalidNumber);
+  let annualPercantageRate = retriveInput(messages.intRate, messages.wrongInput, isInvalidAPR);
 
-  let loanDurationYears = retriveInput(messages.loanTerm, messages.wrongInput, isInvalidNumber);
-
-  let countMonthlyInterestRate = (annRate) => (annRate / 100) / 12;
-
-  let countMonthlyPayment = (loanAmount, monthlyInterestRate, loanDurationYears) => {
-    return Number(((loanAmount * (monthlyInterestRate /
-      (1 - Math.pow(1 + monthlyInterestRate, -(loanDurationYears * 12))))).toFixed(2)));
-  }
-
-  let countTotalPayment = (paymentMonth, loanTerm) => {
-    return (paymentMonth * (loanTerm * 12));
-  }
+  let loanDurationYears = retriveInput(messages.loanTerm, messages.wrongYears, isInvalidNumber);
 
   let monthlyInterestRate = countMonthlyInterestRate(annualPercantageRate);
   let monthlyPayment = countMonthlyPayment(loanAmount, monthlyInterestRate, loanDurationYears);
   let totalPayment = countTotalPayment(monthlyPayment, loanDurationYears);
-  let totalInterest = (totalPayment - loanAmount);
+  let totalInterest = countTotalInterest(totalPayment, loanAmount);
 
   printResult(messages.paymentMonth, monthlyPayment.toLocaleString());
   printResult(messages.paymentTotal, totalPayment.toLocaleString());
   printResult(messages.interestTotal, totalInterest.toLocaleString());
 
-  countAnother = retriveInput(messages.continue, messages.invalid, isInvalidInput);
-
-  if (countAnother[0].toLowerCase() !== "y") break;
+  let countAnother = retriveInput(messages.continue, messages.invalid, isInvalidInput);
+  clearScreen();
+  if (countAnother[0].toLowerCase() !== "y") {
+    break;
+  }
 }
